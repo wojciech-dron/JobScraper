04aa307251b6178a68ed12f270195c62c074fe0a
Wojciech Dron <dronw96@gmail.com>
2025-02-17 20:29:49 +0100

    Allowing errors in IndeedListScraper

--- JobScraper/Logic/Indeed/IndeedList.cs
+++ JobScraper/Logic/Indeed/IndeedList.cs
@@ -24,11 +24,19 @@ public class IndeedList
         [Command("indeed-list")]
         public async Task Handle(Scrape? scrape = null, CancellationToken cancellationToken = default)
         {
-            _logger.LogInformation("Scraping Indeed jobs list...");
-            await foreach (var jobs in _list.ScrapeJobs().WithCancellation(cancellationToken))
+            try
             {
-                _logger.LogInformation("Syncing {JobsCount} scraped jobs...", jobs.Count);
-                await _mediator.Send(new SyncJobsFromList.Command(jobs), cancellationToken);
+                _logger.LogInformation("Scraping Indeed jobs list...");
+                await foreach (var jobs in _list.ScrapeJobs().WithCancellation(cancellationToken))
+                {
+                    _logger.LogInformation("Syncing {JobsCount} scraped jobs...", jobs.Count);
+                    await _mediator.Send(new SyncJobsFromList.Command(jobs), cancellationToken);
+                }
+            }
+            catch (ApplicationException e)
+            {
+                _logger.LogError(e, "Error occured during scaping indeed." +
+                    " Probably retry exceeded after cloudflare human verification");
             }
         }
     }
