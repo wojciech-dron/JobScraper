89f86afa6e62ec65754702b9c8ba5b61adb3db27
Wojciech Dron <wojciech.dron@primaris.eu>
2025-04-30 12:42:27 +0200

    filtering without click

---
+++ JobScraper.Web/Extensions/QuickGridExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components.QuickGrid;
+
+namespace JobScraper.Web.Extensions;
+
+public static class QuickGridExtensions
+{
+    public static async Task SortAsync<T>(this ColumnBase<T> column) =>
+        await column.Grid.SortByColumnAsync(column);
+
+}
