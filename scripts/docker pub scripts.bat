7b80be7bb5d3a08aa6e09216aed9af7c28d38bd0
Wojciech Dron <dronw96@gmail.com>
2025-07-09 17:47:38 +0200

    Release 0.2.2

--- scripts/docker pub scripts.bat
+++ scripts/docker pub scripts.bat
@@ -3,5 +3,5 @@ docker login
 docker tag jobscraper.web combi71/jobscraper.web:latest
 docker push combi71/jobscraper.web:latest
 
-docker tag jobscraper.web combi71/jobscraper.web:0.2.1
-docker push combi71/jobscraper.web:0.2.1
+docker tag jobscraper.web combi71/jobscraper.web:0.2.2
+docker push combi71/jobscraper.web:0.2.2
