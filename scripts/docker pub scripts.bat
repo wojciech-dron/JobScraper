5f1753ea66335a71b5ac5d7f41c4223a2ebced57
Wojciech Dron <dronw96@gmail.com>
2025-12-02 19:26:13 +0100

    Optimizing dockerfile

--- scripts/docker pub scripts.bat
+++ scripts/docker pub scripts.bat
@@ -1,7 +1,11 @@
 docker login
 
+docker build -t jobscraper.web -f Jobscraper.Web/Dockerfile
+
 docker tag jobscraper.web combi71/jobscraper.web:latest
 docker push combi71/jobscraper.web:latest
 
-docker tag jobscraper.web combi71/jobscraper.web:0.2.2
-docker push combi71/jobscraper.web:0.2.2
+docker tag jobscraper.web combi71/jobscraper.web:0.3
+docker push combi71/jobscraper.web:0.3
+
+dive jobscraper.web:latest
