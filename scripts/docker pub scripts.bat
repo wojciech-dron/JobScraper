836cf1273f26d554deb3f9143d3dfcc1a1cf4855
Wojciech Dron <dronw96@gmail.com>
2025-12-08 20:49:03 +0100

    update scripts

--- scripts/docker pub scripts.bat
+++ scripts/docker pub scripts.bat
@@ -5,7 +5,7 @@ docker build -t jobscraper.web -f Jobscraper.Web/Dockerfile
 docker tag jobscraper.web combi71/jobscraper.web:latest
 docker push combi71/jobscraper.web:latest
 
-docker tag jobscraper.web combi71/jobscraper.web:0.3
-docker push combi71/jobscraper.web:0.3
+docker tag jobscraper.web combi71/jobscraper.web:0.4
+docker push combi71/jobscraper.web:0.4
 
 dive jobscraper.web:latest
