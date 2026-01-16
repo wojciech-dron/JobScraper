46396161eed683206f1a711e6bb3a1340f3b825f
Wojciech Dron <dronw96@gmail.com>
2026-01-16 16:00:52 +0100

    Adding auth

--- scripts/ef-core.ps1
+++ scripts/ef-core.ps1
@@ -2,7 +2,7 @@
 dotnet ef migrations add --project JobScraper.Web/JobScraper.Web.csproj [Name]
 
 # Updating database
-dotnet ef database update --project JobScraper.Web/JobScraper.Web.csproj [Name]
+dotnet ef database update --project JobScraper.Web/JobScraper.Web.csproj --connection "data source=..\\Data\\Jobs.db"
 
 # Remove migration
 dotnet ef migrations remove --project JobScraper.Web/JobScraper.Web.csproj [Name]
