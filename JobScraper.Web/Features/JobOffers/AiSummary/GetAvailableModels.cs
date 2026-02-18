e56602c44d8f93c2b8835ab2ac6fdc80d6bc4be8
Wojciech Dron <dronw96@gmail.com>
2026-02-18 17:41:13 +0100

    Httpclient + serilog

---
+++ JobScraper.Web/Features/JobOffers/AiSummary/GetAvailableModels.cs
@@ -0,0 +1,38 @@
+﻿using ErrorOr;
+using Mediator;
+
+namespace JobScraper.Web.Features.JobOffers.AiSummary;
+
+public class GetAvailableModels
+{
+    public record Request(AiSummaryConfig Config) : IRequest<ErrorOr<Response>>;
+
+    public record Response(ModelsDto[] Models);
+
+    internal class Handler(IHttpClientFactory clientFactory) : IRequestHandler<Request, ErrorOr<Response>>
+    {
+        public async ValueTask<ErrorOr<Response>> Handle(Request request, CancellationToken cancellationToken)
+        {
+            var config = request.Config;
+
+            var client = clientFactory.CreateClient();
+            client.BaseAddress = new Uri(config.BaseUrl);
+
+            var response = await client.GetAsync("api/v1/models", cancellationToken);
+
+            if (!response.IsSuccessStatusCode)
+                return Error.Unexpected(description: $"Server returned error code: {response.StatusCode}");
+
+            var responseContent = await response.Content.ReadFromJsonAsync<ResponseDto>(cancellationToken);
+
+            if (responseContent is null)
+                return Error.Unexpected(description: "Failed to deserialize response");
+
+            return new Response(responseContent.Data);
+        }
+    }
+
+    public record ResponseDto(ModelsDto[] Data);
+
+    public record ModelsDto(string Id, int Created);
+}
---
+++ JobScraper.Web/Modules/Logging/Setup.cs
@@ -0,0 +1,71 @@
+﻿using OpenTelemetry;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using Serilog;
+using Serilog.Extensions;
+using Serilog.Formatting.Display;
+
+namespace JobScraper.Web.Modules.Logging;
+
+public static class Setup
+{
+    public static WebApplicationBuilder AddLogging(this WebApplicationBuilder builder)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .Enrich.FromLogContext()
+            .Enrich.WithMachineName()
+            .Enrich.WithClientIp()
+            .Enrich.WithProcessId()
+            .Enrich.WithRequestQuery()
+            .WriteTo.OpenTelemetry()
+            .WriteTo.Console(new MessageTemplateTextFormatter(
+                "[{Timestamp:HH:mm:ss.fff} {Level:u3} {SourceContext:l} {TraceId:l}] {Message:lj} {NewLine}{Exception}"))
+            .ReadFrom.Configuration(builder.Configuration)
+            .CreateLogger();
+
+        builder.Logging.ClearProviders();
+        builder.Logging.AddSerilog();
+
+        builder.Services.AddOpenTelemetry()
+            .ConfigureResource(c => c.AddService(builder.Environment.ApplicationName))
+            .WithMetrics(metrics =>
+            {
+                metrics.AddAspNetCoreInstrumentation()
+                    .AddHttpClientInstrumentation()
+                    .AddRuntimeInstrumentation();
+            })
+            .WithTracing(tracing =>
+            {
+                tracing.AddSource(builder.Environment.ApplicationName)
+                    .AddAspNetCoreInstrumentation()
+                    .AddHttpClientInstrumentation(); // handling traceparent headers for System.Diagnostics.Activity
+            });
+
+        builder.AddOpenTelemetryExporters();
+
+        return builder;
+    }
+
+    private static TBuilder AddOpenTelemetryExporters<TBuilder>(this TBuilder builder)
+        where TBuilder : IHostApplicationBuilder
+    {
+        var configuration = builder.Configuration;
+
+        var oltpEndpoint = configuration["OtelEndpoint"] ?? configuration["OTEL_EXPORTER_OTLP_ENDPOINT"];
+
+        if (string.IsNullOrWhiteSpace(oltpEndpoint))
+            return builder;
+
+        var protocolString = configuration["OtelProtocol"] ?? configuration["OTEL_EXPORTER_OTLP_PROTOCOL"];
+        var protocol = Enum.TryParse(protocolString, true, out OtlpExportProtocol protocolEnum)
+            ? protocolEnum
+            : OtlpExportProtocol.Grpc;
+
+        builder.Services.AddOpenTelemetry()
+            .UseOtlpExporter(protocol, new Uri(oltpEndpoint));
+
+        return builder;
+    }
+}
