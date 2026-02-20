9bba218ef7291c78b9dd961bc2967aad965990e5
Wojciech Dron <dronw96@gmail.com>
2026-02-20 15:02:06 +0100

    Added Ai sumarry configuration with persistence

---
+++ JobScraper.Web/Common/Entities/AiSummaryConfig.cs
@@ -0,0 +1,34 @@
+﻿using JobScraper.Web.Modules.Persistence.Interceptors;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace JobScraper.Web.Common.Entities;
+
+public class AiSummaryConfig : IOwnable
+{
+    public string? Owner { get; set; } = "system";
+
+    public bool AiSummaryEnabled { get; set; }
+    public string ProviderName { get; set; }
+
+    public string CvContent { get; set; } = "";
+    public string? UserRequirements { get; set; }
+    public string? TestOfferContent { get; set; }
+}
+
+public class AiSummaryConfigModelBuilder : IEntityTypeConfiguration<AiSummaryConfig>
+{
+    public void Configure(EntityTypeBuilder<AiSummaryConfig> builder)
+    {
+        builder.ToTable("AiSummaryConfigs");
+
+        builder.HasKey(x => x.Owner);
+        builder.Property(j => j.Owner).HasMaxLength(255);
+
+        builder.Property(x => x.AiSummaryEnabled);
+        builder.Property(x => x.ProviderName).HasMaxLength(100);
+        builder.Property(x => x.UserRequirements).HasMaxLength(500);
+        builder.Property(x => x.CvContent).HasMaxLength(10_000);
+        builder.Property(x => x.TestOfferContent).HasMaxLength(10_000);
+    }
+}
--- JobScraper.Web/Integration/Setup.cs
+++ JobScraper.Web/Integration/Setup.cs
@@ -1,11 +1,27 @@
-﻿namespace JobScraper.Web.Integration;
+﻿using JobScraper.Web.Integration.AiProvider;
+using Microsoft.Extensions.Options;
+
+namespace JobScraper.Web.Integration;
 
 public static class Setup
 {
     public static WebApplicationBuilder AddIntegrationServices(this WebApplicationBuilder builder)
     {
-        builder.Services.AddHttpClient();
+        AddAiProviders(builder);
 
         return builder;
     }
+
+    private static void AddAiProviders(WebApplicationBuilder builder)
+    {
+        builder.Services.Configure<AiProvidersConfig>(builder.Configuration.GetSection(AiProvidersConfig.SectionBase));
+
+        var providers = builder.Services.BuildServiceProvider()
+            .GetRequiredService<IOptions<AiProvidersConfig>>()
+            .Value;
+
+        foreach (var provider in providers)
+            builder.AddAiProvider(provider.Key);
+    }
+
 }
