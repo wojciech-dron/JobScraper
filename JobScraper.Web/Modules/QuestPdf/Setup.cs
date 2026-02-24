6b7fbfed0986e06cfac4a0ae2bac44bcaf54d17c
Wojciech Dron <dronw96@gmail.com>
2026-02-24 13:06:49 +0100

    Added CV generation (wip)

---
+++ JobScraper.Web/Modules/QuestPdf/Setup.cs
@@ -0,0 +1,14 @@
+﻿using QuestPDF.Infrastructure;
+
+namespace JobScraper.Web.Modules.QuestPdf;
+
+public static class Setup
+{
+    public static WebApplicationBuilder AddQuestPdf(this WebApplicationBuilder builder)
+    {
+        QuestPDF.Settings.License = LicenseType.Community;
+
+        return builder;
+    }
+
+}
