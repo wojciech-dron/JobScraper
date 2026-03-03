48d7038c77d9f288336f4fe48349f26949ddea36
Wojciech Dron <dronw96@gmail.com>
2026-03-03 18:24:35 +0100

    feat: Deleting CVs

---
+++ JobScraper.Web/Modules/Persistence/Migrations/20260303172335_Fix_Cv_FK_Behaviour.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobScraper.Migrations
+{
+    /// <inheritdoc />
+    public partial class Fix_Cv_FK_Behaviour : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cvs_Cvs_OriginCvId",
+                table: "Cvs");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserOffers_Cvs_CvId",
+                table: "UserOffers");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cvs_Cvs_OriginCvId",
+                table: "Cvs",
+                column: "OriginCvId",
+                principalTable: "Cvs",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserOffers_Cvs_CvId",
+                table: "UserOffers",
+                column: "CvId",
+                principalTable: "Cvs",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cvs_Cvs_OriginCvId",
+                table: "Cvs");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserOffers_Cvs_CvId",
+                table: "UserOffers");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cvs_Cvs_OriginCvId",
+                table: "Cvs",
+                column: "OriginCvId",
+                principalTable: "Cvs",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserOffers_Cvs_CvId",
+                table: "UserOffers",
+                column: "CvId",
+                principalTable: "Cvs",
+                principalColumn: "Id");
+        }
+    }
+}
