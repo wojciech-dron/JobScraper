4566b53e7a90bf53c4ba3b44ea013f5be2db37f9
Wojciech Dron <dronw96@gmail.com>
2026-03-07 11:53:58 +0100

    feat: Pdf endpoinf for cv

--- JobScraper.Web/Features/Cv/Setup.cs
+++ JobScraper.Web/Features/Cv/Setup.cs
@@ -5,6 +5,7 @@ public static class Setup
     public static IEndpointRouteBuilder UseCvFeatures(this IEndpointRouteBuilder app)
     {
         app.MapCvImageEndpoints();
+        app.MapCvPdfEndpoints();
 
         return app;
     }
