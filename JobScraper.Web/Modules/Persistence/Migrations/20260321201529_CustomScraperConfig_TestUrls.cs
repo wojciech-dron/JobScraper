0bf3f0aa45c2ab28b457693180d1f1505dd8e7e9
Wojciech Dron <dronw96@gmail.com>
2026-03-21 21:41:09 +0100

    feat: Custom scraper pages

---
+++ JobScraper.Web/Modules/Persistence/Migrations/20260321201529_CustomScraperConfig_TestUrls.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobScraper.Migrations
+{
+    /// <inheritdoc />
+    public partial class CustomScraperConfig_TestUrls : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TestDetailsUrl",
+                table: "CustomScraperConfigs",
+                type: "TEXT",
+                maxLength: 2048,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TestListUrl",
+                table: "CustomScraperConfigs",
+                type: "TEXT",
+                maxLength: 2048,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TestDetailsUrl",
+                table: "CustomScraperConfigs");
+
+            migrationBuilder.DropColumn(
+                name: "TestListUrl",
+                table: "CustomScraperConfigs");
+        }
+    }
+}
